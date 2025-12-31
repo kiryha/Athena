d0157eaa78ff41ec2ff749ad49e7154d05debb68
Kiryha <kiryha@gmail.com>
2025-12-31 01:34:40 -0500

    wip

---
+++ rnd/snippets.py
@@ -0,0 +1,9 @@
+"""
+Unsorted pieces of code
+"""
+
+
+import torch
+print(torch.cuda.is_available())
+print(torch.__version__)
+
--- rnd/text-to-image.py
+++ rnd/text-to-image.py
@@ -1,10 +1,15 @@
 """
 Hello World AI image generation
+
+# To uninstall: pip uninstall -y torch torchvision torchaudio xformers
+
+# Prerequisites:
+pip install torch==2.5.1+cu121 torchvision==0.20.1+cu121 torchaudio==2.5.1+cu121 --index-url https://download.pytorch.org/whl/cu121
+
 """
 
 import torch
 from diffusers import StableDiffusionXLPipeline
-
 
 model_path = "E:/Projects/ComfyUI_windows_portable/ComfyUI/models/checkpoints/juggernautXL_juggXIByRundiffusion.safetensors"
 prompt = "photorealistic robot with yellow painted metal"
@@ -13,10 +18,13 @@ output_image = "E:/robot_001.png"
 
 pipeline = StableDiffusionXLPipeline.from_single_file(
     model_path,
-    torch_dtype=torch.float16
+    torch_dtype=torch.float16,
+    use_safetensors=True
 )
 
 pipeline.to("cuda")
+pipeline.enable_attention_slicing()
+# pipeline.enable_xformers_memory_efficient_attention()
 
-image = pipeline(prompt).images[0]
+image = pipeline(prompt, num_inference_steps=20).images[0]
 image.save(output_image)
