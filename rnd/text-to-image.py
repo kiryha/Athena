53f88f78139d271c2d69959a083e419655a099d1
Kiryha <kiryha@gmail.com>
2025-12-31 02:06:52 -0500

    switch to small model

--- rnd/text-to-image.py
+++ rnd/text-to-image.py
@@ -6,25 +6,26 @@ Hello World AI image generation
 # Prerequisites:
 pip install torch==2.5.1+cu121 torchvision==0.20.1+cu121 torchaudio==2.5.1+cu121 --index-url https://download.pytorch.org/whl/cu121
 
+# RUN THIS IN CMD: before running the script!!!
+setx HF_HUB_DISABLE_SYMLINKS_WARNING 1
 """
 
 import torch
-from diffusers import StableDiffusionXLPipeline
+from diffusers import StableDiffusionPipeline
 
-model_path = "E:/Projects/ComfyUI_windows_portable/ComfyUI/models/checkpoints/juggernautXL_juggXIByRundiffusion.safetensors"
+
+# model_path = "E:/Projects/ComfyUI_windows_portable/ComfyUI/models/checkpoints/juggernautXL_juggXIByRundiffusion.safetensors"
+model_path = "E:/Projects/ComfyUI_windows_portable/ComfyUI/models/checkpoints/v1-5-pruned-emaonly-fp16.safetensors"
 prompt = "photorealistic robot with yellow painted metal"
 output_image = "E:/robot_001.png"
 
-
-pipeline = StableDiffusionXLPipeline.from_single_file(
+pipe = StableDiffusionPipeline.from_single_file(
     model_path,
     torch_dtype=torch.float16,
     use_safetensors=True
 )
 
-pipeline.to("cuda")
-pipeline.enable_attention_slicing()
-# pipeline.enable_xformers_memory_efficient_attention()
+pipe.to("cuda")
 
-image = pipeline(prompt, num_inference_steps=20).images[0]
+image = pipe(prompt,  num_inference_steps=4).images[0]
 image.save(output_image)
